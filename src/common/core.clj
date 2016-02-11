b146d258a5547be14807c3024843ed5d6fd36f77
James Borden <jmborden@gmail.com>
2016-02-11 11:08:24 -0600

    initial commit

---
+++ src/common/core.clj
@@ -0,0 +1,6 @@
+(ns common.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/common/core_test.clj
@@ -0,0 +1,7 @@
+(ns common.core-test
+  (:require [clojure.test :refer :all]
+            [common.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
